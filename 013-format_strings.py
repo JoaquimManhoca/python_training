72c9ecc756129f705ee6551adc81b0ff7f2fe530
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 18:55:04 +0200

    work with string format

---
+++ 013-format_strings.py
@@ -0,0 +1,10 @@
+#String Format
+age = 26
+text = f"My name is Joaquim, I am {age}"
+print(text)
+
+#Placeholders and Modifiers
+num1 = 2.3232
+num2 = 3.53232
+some = num1 + num2
+print(f"{num1:.2f} + {num2:.2f} = {some:.2f}")
