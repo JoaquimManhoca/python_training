71aefd21789ef8454ca239364e0e6875e906e681
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 20:59:47 +0200

    work with Lists

---
+++ 016-list.py
@@ -0,0 +1,17 @@
+mylist = ['banana', 'orange', 'apple']
+
+#Allow Duplicates
+fruts_list = ['banana', 'banana', 'apple']
+
+#List Length
+print(len(fruts_list))
+
+#A list can contain different data types
+list1 = ["abc", 34, True, 40, "male"]
+
+#type()
+print(type(list1))
+
+#The list() Constructor
+num = list((1,2,3))
+print(num)
