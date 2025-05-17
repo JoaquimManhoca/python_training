34afe2d2a2eed789b1525177400a2b411e4def2a
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 19:02:03 +0200

    work with escape character

---
+++ 014-escape_characters.py
@@ -0,0 +1,16 @@
+#Escape Characters
+
+"""
+\'	Single Quote
+\\	Backslash
+\n	New Line
+\r	Carriage Return
+\t	Tab
+\b	Backspace
+\f	Form Feed
+\ooo	Octal value
+\xhh	Hex value
+
+"""
+
+print("I'm training to became \"Machene learning engineer\" ")
