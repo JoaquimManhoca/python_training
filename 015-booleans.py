9c6674f99cfd1e6142af272a821b41a93c17b674
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 19:54:24 +0200

    work with booleans

---
+++ 015-booleans.py
@@ -0,0 +1,13 @@
+#booleans
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# the if condition return a boolean
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
