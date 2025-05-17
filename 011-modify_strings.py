b3c84ebad7247aa12422927e10cb067fff8d3656
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 18:25:40 +0200

    Used differents strings modifiers

---
+++ 011-modify_strings.py
@@ -0,0 +1,16 @@
+#Upper Case
+name = "Joaquim"
+print(name.upper())
+
+#Lower Case
+print(name.lower())
+
+#Remove Whitespace
+full_name = "  Joaquim Adriano Manhoca"
+print(full_name.strip())
+
+#Replace String
+print(name.replace('J', 'K'))
+
+#Split String
+print(full_name.split(' '))
