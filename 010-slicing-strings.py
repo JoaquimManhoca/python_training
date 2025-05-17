1e4ceaebd14232eadb745e036e07458e0ccd5680
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 14:39:16 +0200

    defferent ways to Slicing a sting

---
+++ 010-slicing-strings.py
@@ -0,0 +1,13 @@
+#Slicing
+name = "joaquim"
+print(name[0:4])
+
+#Slice From the Start
+print(name[:4])
+
+#Slice To the End
+print(name[3:])
+
+#Negative Indexing
+greet = "Hello, World!"
+print(greet[-5:-2])
