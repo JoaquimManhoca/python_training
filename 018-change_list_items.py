dba34a58bee156c20c4282fdcd8fb690c4e60b19
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-17 21:28:13 +0200

    work with list to change list items

---
+++ 018-change_list_items.py
@@ -0,0 +1,14 @@
+#Chenge Item Value
+thislist = ["apple", "banana", "cherry"]
+thislist[2] = "Mango"
+print(thislist)
+
+#Change a Range of Item Values
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[2:4] = ['Tomatos', 'onion']
+print(thislist)
+
+#insert items
+thislist2 = ["apple", "banana", "cherry"]
+thislist2.insert(2, "watermelon")
+print(thislist2)
