eeefc99e77c26de214afc31dc8e872f7c246df29
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-19 17:27:41 +0200

    work with delete list method

---
+++ 020-remove_list_items.py
@@ -0,0 +1,16 @@
+#Remove Specified Item
+fruts = ['orange', 'mango', 'apple']
+fruts.remove('apple')
+print(fruts)
+
+#Remove Specified Index
+fruts.extend(['watermelon', 'Tomatos', 'banana', 'uva'])
+fruts.pop(2)
+print(fruts)
+
+#delete all list
+del fruts
+
+#Clear the List
+fruts = (['watermelon', 'Tomatos', 'banana', 'uva'])
+fruts.clear()
