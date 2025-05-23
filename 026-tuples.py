89c764d744e1f7f726a3b5805ab0c653a1ae4c44
joaquim_manhoca <joaquimmanhoca222@gmail.com>
2025-05-23 20:03:20 +0200

    intro to work with build in data type tuple

---
+++ 026-tuples.py
@@ -0,0 +1,24 @@
+mytuple = ("apple", "banana", "cherry")
+
+#Allow Duplicates
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#Tuple Length
+print(len(thistuple))
+
+#Create Tuple With One Item
+name = ('joaquim',)
+print(f"{name} is type {type(name)}")
+
+#Tuple Items - Data Types
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#A tuple can contain different data types
+tuple1 = ("abc", 34, True, 40, "male")
+
+#The tuple() Constructor
+thisistuple = tuple(("apple", "banana", "cherry"))
+print(thisistuple)
